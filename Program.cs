5b3d08d19cb18920179175610802442cf2b508d8
David <dlarsen13@my.wctc.edu>
2023-02-18 18:03:40 -0600

    [1.07] Program Loop, Check for data.txt in parse

--- Program.cs
+++ Program.cs
@@ -1,49 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿string dir = Directory.GetCurrentDirectory();
+Boolean running = true;
 // ask for input
-Console.WriteLine("Enter 1 to create data file.");
-Console.WriteLine("Enter 2 to parse data.");
-Console.WriteLine("Enter anything else to quit.");
-// input response
-string? resp = Console.ReadLine();
-
-if (resp == "1")
+while(running)
 {
-            // create file
-    StreamWriter sw = new StreamWriter("data.txt");
+    Console.WriteLine("Enter 1 to create data file.");
+    Console.WriteLine("Enter 2 to parse data.");
+    Console.WriteLine("Enter anything else to quit.");
+    // input response
+    string? resp = Console.ReadLine();
 
-    // ask a question
-    Console.WriteLine("How many weeks of data is needed?");
-    // input the response (convert to int)
-    int weeks = int.Parse(Console.ReadLine());
-    // determine start and end date
-    DateTime today = DateTime.Now;
-    // we want full weeks sunday - saturday
-    DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
-    // subtract # of weeks from endDate to get startDate
-    DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
-    Console.WriteLine(dataDate);
-    // random number generator
-    Random rnd = new Random();
-
-    // loop for the desired # of weeks
-    while (dataDate < dataEndDate)
+    if (resp == "1")
     {
-        // 7 days in a week
-        int[] hours = new int[7];
-        for (int i = 0; i < hours.Length; i++)
-        {
-            // generate random number of hours slept between 4-12 (inclusive)
-            hours[i] = rnd.Next(4, 13);
-        }
-        // M/d/yyyy,#|#|#|#|#|#|#
-        sw.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
-        // add 1 week to date
-        dataDate = dataDate.AddDays(7);
-    }
-    sw.Close();
-}
-else if (resp == "2")
-{
-    // TODO: parse data file
+        // create file
+        StreamWriter sw = new StreamWriter("data.txt");
 
+        // ask a question
+        Console.WriteLine("How many weeks of data is needed?");
+        // input the response (convert to int)
+        int weeks = int.Parse(Console.ReadLine());
+        // determine start and end date
+        DateTime today = DateTime.Now;
+        // we want full weeks sunday - saturday
+        DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
+        // subtract # of weeks from endDate to get startDate
+        DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
+        Console.WriteLine(dataDate);
+        // random number generator
+        Random rnd = new Random();
+
+        // loop for the desired # of weeks
+        while (dataDate < dataEndDate)
+        {
+            // 7 days in a week
+            int[] hours = new int[7];
+            for (int i = 0; i < hours.Length; i++)
+            {
+                // generate random number of hours slept between 4-12 (inclusive)
+                hours[i] = rnd.Next(4, 13);
+            }
+            // M/d/yyyy,#|#|#|#|#|#|#
+            sw.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
+            // add 1 week to date
+            dataDate = dataDate.AddDays(7);
+        }
+        sw.Close();
+    }
+    else if (resp == "2")
+    {
+        if (File.Exists(dir + "\\data.txt"))
+        {
+            Console.WriteLine("File found!");
+            StreamReader sr = new StreamReader(dir + "\\data.txt");
+            
+        }
+        else{
+            Console.WriteLine("Error: No data file found");
+        }
+    }
+    else
+        running = false;
 }
